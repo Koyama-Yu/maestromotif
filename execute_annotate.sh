df96e4ce2f9ab16a3df6ba2d2757d702b4af0910
Koyama-Yu <xiaoshanyu427@gmail.com>
2025-03-11 16:03:50 +0900

    test evaluation & random annotation

---
+++ execute_annotate.sh
@@ -0,0 +1,6 @@
+python -m scripts.annotate_pairs_ult --annotator_type random \
+                                 --directory og_dataset \
+                                 --custom_annotator_string random_annotate_test \
+                                 --n_chunks 1 \
+                                 --chunk_number 0 \
+                                 --ignore_existing 1
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from maestromotif!")
+
+
+if __name__ == "__main__":
+    main()
