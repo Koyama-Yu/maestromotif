abe32dcd2493c6ad307f871c08b55b233032f9d0
Koyama-Yu <xiaoshanyu427@gmail.com>
2025-08-27 23:40:49 +0000

    complete pureRL test

---
+++ execute_evaluate_pureRL.sh
@@ -0,0 +1,12 @@
+python -m scripts.main --algo APPO --num_workers 24 --num_envs_per_worker 20 \
+                       --batch_size 4096 --reward_scale 0.1 --obs_scale 255.0 \
+                       --train_for_env_steps 5_000_000_000 \
+                       --save_every_steps 6_000_000_000 \
+                       --keep_checkpoints 5 --stats_avg 1000 --seed 2 --code_seed -1 \
+                       --reward_dir "[]" \
+                       --extrinsic_reward 1.0 --llm_reward 0.0 \
+                       --train_dir train_dir/skill_policy/ \
+                       --experiment pureRL \
+                       --eval_target none \
+                       --ttyrec 1 \
+                       --evaluation True
